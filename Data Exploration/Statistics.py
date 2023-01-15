485ee604f42d613d1269d402c2d7d0fce3c9ac73
solka <solka.ola@gmail.com>
2023-01-15 20:24:52 +0100

    Classes for the program

---
+++ Data Exploration/Statistics.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+
+class Statistics:
+    def __init__(self):
+        self.df = pd.read_csv(r"https://github.com/OlaSolka/Titanic-analysis/blob/c2a442ebab23aecd292f4e3985da63a51efbafb2/train.csv")
+
+
