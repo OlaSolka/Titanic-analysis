5585fc971d451755f6e8d41e48cd0b7062682e62
solka <solka.ola@gmail.com>
2023-01-29 19:52:06 +0100

    final version

--- Analysis/age_histogram.py
+++ Analysis/age_histogram.py
@@ -6,12 +6,6 @@ import pandas as pd
 df = pd.read_csv(r"C:\Users\solka\PycharmProjects\Titanic-Analysis/train.csv")
 
 df['Age'].plot(kind='hist', bins=20, color='grey')
-# df["Age"] = df["Age"].fillna(-0.5)
-#
-# bins = [-1, 0, 5, 12, 18, 24, 35, 60, 100]
-# labels = ['Unknown', 'Baby', 'Child', 'Teenager', 'Student', 'Young Adult', 'Adult', 'Senior']
-# df['AgeGroup'] = pd.cut(df["Age"], bins, labels=labels)
-# df['AgeGroup'].plot(kind='hist', bins=bins, labels=labels)
 plt.show()
 
 # In the plot we can se that it is almost normal distribution except the one extreme value which is the youngest group.
